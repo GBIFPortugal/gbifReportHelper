86d204f1e647f1241ae004ea7227b3f94619db88
Rui Figueira <ruifigueira@isa.ulisboa.pt>
2020-04-30 23:24:33 +0100

    add arguments and option to get datasets for organizations

--- gbifStat_datasetsTable.py
+++ gbifStat_datasetsTable.py
@@ -28,6 +28,7 @@ import sys
 import json
 import requests
 import csv
+import argparse
 
 
 # obtain number of downloads and number of downloaded records for the dataset
@@ -75,7 +76,7 @@ def gbifDownload(datasetKey, yearStats):
     return (countDwl, countRec)
 
 # obtain the list of datasets for the node
-def gbifGetDatasets(nodeKey):
+def gbifGetNodeDatasets(nodeKey):
     endOfRecords = False
     offset = 0
     limit = 20
@@ -99,21 +100,68 @@ def gbifGetDatasets(nodeKey):
                 i += 1
             offset += 20
         except ValueError:
-            print ('ValueError on gbifGetDatasets')
+            print ('ValueError on gbifGetNodeDatasets')
+    return listaDatasets
+
+# obtain the list of datasets for the institution
+def gbifGetOrgDatasets(institutionKey):
+    endOfRecords = False
+    offset = 0
+    limit = 20
+    address = 'http://api.gbif.org/v1/organization/' + institutionKey + '/publishedDataset'
+    print (address)
+    listaDatasets = []
+    while endOfRecords == False:
+        try:
+            parms = {'offset': offset, 'limit': limit}
+            try:
+                txt = requests.get(address, params = parms)
+            except IOError:
+                print ('problema no acesso ao GBIF : código:', txt.raise_for_status())
+            parsed_json = json.loads(txt.text)
+            endOfRecords = parsed_json['endOfRecords']
+            results = parsed_json['results']
+            i = 0
+            for x in results:
+                line = [results[i]["key"], results[i]["type"], results[i]["publishingOrganizationKey"], results[i]["title"], results[i]["created"], results[i]["modified"], results[i]["license"]]
+                listaDatasets.append(line)
+                i += 1
+            offset += 20
+        except ValueError:
+            print ('ValueError on gbifGetOrgDatasets')
     return listaDatasets
 
 
+# Obtain the number of records of a dataset
+def gbifGetDatasetCount(datasetKey):
+    address = 'https://api.gbif.org/v1/occurrence/search?datasetKey=' + datasetKey
+    try:
+        txt = requests.get(address)
+    except IOError:
+        print ('problema no acesso ao API GBIF em gbifGetDatasetCount: código:', txt.raise_for_status())
+    parsed_json = json.loads(txt.text)
+    count = parsed_json['count']
+    return count
+
+
+
 def main():
-    if len(sys.argv) >= 3:
-        nodeKey = sys.argv[1]
-        yearRelat = sys.argv[2]
-    else:
-        print ("Run command: python gbifStat_dataset.py [nodeKey] [year]\nExample: python gbifStat_dataset.py 673f7038-4262-4149-b753-5658a4e912f6 2019")
-        nodeKey = '673f7038-4262-4149-b753-5658a4e912f6'
-        yearRelat = 2019
-    listaDatasets = gbifGetDatasets(nodeKey)
-    print ("Existem " + str(len(listaDatasets)) + "datasets.")
-    filename = "stats_count_" + nodeKey + ".csv"
+
+    parser = argparse.ArgumentParser(description = "Help on how to use this script ")
+    parser.add_argument("-n", "--nodeKey", help = "GUID of the node. Example: 673f7038-4262-4149-b753-5658a4e912f6", required = False, default = "673f7038-4262-4149-b753-5658a4e912f6")
+    parser.add_argument("-o", "--organizationKey", help = "GUID of the organization. Example: dfad10f6-d84b-4fa2-b6d0-df2a3eb38b65", required = False, default = "dfad10f6-d84b-4fa2-b6d0-df2a3eb38b65")
+    parser.add_argument("-y", "--year", type=int, help = "Year of report. Example: 2019", required = False, default = "2019")
+
+    argument = parser.parse_args()
+    status = False
+    print(argument.year)
+    print(argument.nodeKey)
+
+    yearRelat = argument.year
+    listaDatasets = gbifGetNodeDatasets(argument.nodeKey)
+    listaDatasets_1 = gbifGetOrgDatasets(argument.organizationKey)
+    listaDatasets += listaDatasets_1
+    filename = "stats_count_" + argument.nodeKey + ".csv"
     i = 0
     with open(filename, "w") as file:
         file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -127,8 +175,9 @@ def main():
                 # if datasest are of type Occurrence or Sampling Event
                 if str(lista[1]) == "OCCURRENCE" or str(lista[1]) == "SAMPLING_EVENT":
                     print (lista[1])
+                    numberRecords = gbifGetDatasetCount(lista[0])
                     counts =  gbifDownload(lista[0], yearRelat)
-                    line = (i, lista[2], lista[0], lista[6], lista[3], lista[1],lista[5][0:10],counts[0], counts[1])
+                    line = (i, lista[2], lista[0], lista[6], lista[3], lista[1], lista[5][0:10], numberRecords, counts[0], counts[1])
                     file_writer.writerow(line)
                 else:
                     print (lista[1])
